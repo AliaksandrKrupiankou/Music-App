ae2770c031cde553585efa2344947edd45d017b1
Aleksandr <alekskrupenkov@gmail.com>
2025-12-24 18:40:39 +0300

    feat: setting up a content grid and creating base paths

---
+++ music-app/src/app/core/main-layout-component/main-layout-component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+import { SidebarComponent } from "./sidebar-component/sidebar-component";
+import { PlayerBarComponent } from "./player-bar-component/player-bar-component";
+
+@Component({
+  selector: 'app-main-layout-component',
+  imports: [RouterOutlet, SidebarComponent, PlayerBarComponent],
+  templateUrl: './main-layout-component.html',
+  styleUrl: './main-layout-component.css',
+})
+export class MainLayoutComponent {
+
+}
