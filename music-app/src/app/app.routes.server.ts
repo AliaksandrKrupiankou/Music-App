4a97e6214308725f7d73b6a4778bfb529d3a4e79
unknown <aliaksandr.krupiankou@innowise.com>
2026-03-18 15:49:01 +0300

    fix: fix deploy bug

--- music-app/src/app/app.routes.server.ts
+++ music-app/src/app/app.routes.server.ts
@@ -2,7 +2,24 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'artist/:artistId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'artist/:artistId/tracks',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'albums/:albumId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'playlist/:playlistId',
+    renderMode: RenderMode.Server,
+  },
+
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
